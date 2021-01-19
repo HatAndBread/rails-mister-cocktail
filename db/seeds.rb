3e77d06f5be3ecc6f709aa4b12a07fe8400ed9f4
Joshua Hume <valencelectron@hotmail.com>
2021-01-19 19:31:11 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 
 
-# json = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
-# cocktail_hash = JSON.parse(json) 
-# ingredients = cocktail_hash["drinks"]
+json = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+cocktail_hash = JSON.parse(json) 
+ingredients = cocktail_hash["drinks"]
 
-# ingredients.each_with_index do |ingredient, index|
-#     name = ingredients[index]["strIngredient1"]
-#     ing = Ingredient.new
-#     ing.name = name
-#     ing.save
-# end
+ingredients.each_with_index do |ingredient, index|
+    name = ingredients[index]["strIngredient1"]
+    ing = Ingredient.new
+    ing.name = name
+    ing.save
+end
 
-# doses = ['a sprinkle of', 'a thimble full of', 'a liter of', 'a ton of', 'a shit ton of']
-# doses.each do |dose|
-#     Dose.create!(descr)
-# end
+doses = ['a sprinkle of', 'a thimble full of', 'a liter of', 'a ton of', 'a shit ton of']
+doses.each do |dose|
+    Dose.create!(descr)
+end
