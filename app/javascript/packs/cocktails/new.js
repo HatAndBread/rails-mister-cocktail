3579314b69db9942c3cd3db0d35ecefd607a05b2
Joshua Hume <valencelectron@hotmail.com>
2021-01-20 16:26:05 +0900

    a little bit of style

--- app/javascript/packs/cocktails/new.js
+++ app/javascript/packs/cocktails/new.js
@@ -2,7 +2,6 @@ import c from 'chinpunkanpun';
 
 const butt = document.getElementById('random-name-butt');
 const field = document.querySelector('#text-field');
-console.log(field);
 
 butt.addEventListener('click', () => {
   if (Math.floor(Math.random() * 2)) {
