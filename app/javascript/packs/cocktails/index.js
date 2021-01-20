e2aa9ca944929b3d100f19fdb09692a011b00645
Joshua Hume <valencelectron@hotmail.com>
2021-01-20 16:49:48 +0900

    styled

--- app/javascript/packs/cocktails/index.js
+++ app/javascript/packs/cocktails/index.js
@@ -1,3 +1,10 @@
 import c from 'chinpunkanpun';
 const subTitle = document.querySelector('.subtitle');
+const descriptions = Array.from(document.querySelectorAll('.description'));
 subTitle.innerText = c.sentence();
+
+console.log(descriptions);
+
+descriptions.forEach((description) => {
+  description.innerText = c.sentence() + ' ' + c.sentence();
+});
